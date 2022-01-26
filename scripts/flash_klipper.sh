82ff106c81e316b254348f497c39217b8cb3b3af
Jason Ragsdale <jrags@jasrags.net>
2022-01-26 14:15:42 -0600

    Updates

--- scripts/flash_klipper.sh
+++ scripts/flash_klipper.sh
@@ -2,13 +2,13 @@
 sudo service klipper stop
 cd ~/klipper
 
-# Flash MCU (Spider)
-make clean KCONFIG_CONFIG=config.spider
-make menuconfig KCONFIG_CONFIG=config.spider
-make KCONFIG_CONFIG=config.spider
-read -p "Spider firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
-read -p "Spider firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+# Flash MCU (OctoPro)
+make clean KCONFIG_CONFIG=config.octo
+make menuconfig KCONFIG_CONFIG=config.octo
+make KCONFIG_CONFIG=config.octo
+read -p "OctoPro firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f446xx_470027000550534E4E313120-if00 btt-skr-v1.4
+read -p "OctoPro firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 # Flash MCU (Linux)
 make clean KCONFIG_CONFIG=config.rpi
@@ -27,3 +27,5 @@ read -p "RPi firmware flashed, please check above for any errors. Press [Enter] 
 # read -p "Expander firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
 sudo service klipper start
+
+# make clean KCONFIG_CONFIG=config.octo && make menuconfig KCONFIG_CONFIG=config.octo && make KCONFIG_CONFIG=config.octo
